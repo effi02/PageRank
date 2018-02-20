426e85922d1f7023465ec27cee4de2d86a89f668
effi02 <33747317+effi02@users.noreply.github.com>
2018-02-21 00:10:42 +0100

    Update pr.cpp

--- pr.cpp
+++ pr.cpp
@@ -4,15 +4,15 @@
 void kiir (double tomb[], int db)
 {
 int i;
-for (i=0; i<db; i++)
-printf("PageRank [%d]: %lf\n", i, tomb[i]);
+   for (i=0; i<db; i++)
+	printf("PageRank [%d]: %lf\n", i, tomb[i]);
 }
 
 double tavolsag (double PR[], double PRv[], int n)
 {
 double tav = 0.0;
 int i;
-for (i=0; i< ; i++)
+   for (i=0; i< ; i++)
 	tav += (PRv[i] - PR[i]) * (PRv[i] - PR[i]);
 
 return sqrt(tav);
@@ -39,11 +39,10 @@ for(;;)
 	for (i=0; i<4; ++i)
 	{
 		PR[i] = 0.0;
-		for(j=0; j<4; ++j)
-			PR[i] += (L[i][j] * PRv[j]);
+        		for(j=0; j<4; ++j)
+	                	PR[i] += (L[i][j] * PRv[j]);
 	}
-
-
+	
  if (tavolsag (PR, PRv, 4) < 0.0000000001)
   break;
 
@@ -51,7 +50,6 @@ for(;;)
 		PRv[i] = PR[i];
 
 }
-
 kiir (PR, 4);
 return 0;
 }
